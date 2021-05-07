7537a2b0d1a503f1aa7f5b6db6fb462550613ce2
Nurhanfront <front@studionomad.kz>
2021-05-07 16:06:42 +0600

    crypro refactoring

---
+++ src/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+   Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque modi numquam commodi architecto odio repellat ea ut reprehenderit exercitationem consequuntur eius explicabo asperiores, necessitatibus inventore accusamus quis ipsam alias repellendus.
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
